cac6da4f8eb8ea24dd02f6259fe72d5c8c2e1686
Shuuei Takahashi <takasyue1023sw@gmail.com>
2018-07-09 11:53:57 +0900

    image(1)

---
+++ Design image/js/pulldown.js
@@ -0,0 +1,13 @@
+$(function () {
+    // プルダウン変更時に遷移
+    $('select[name=pulldown1]').change(function () {
+        if ($(this).val() != '') {
+            window.location.href = $(this).val();
+        }
+    });
+    $('select[name=pulldown2]').change(function () {
+        if ($(this).val() != '') {
+            window.location.href = $(this).val();
+        }
+    });
+});
